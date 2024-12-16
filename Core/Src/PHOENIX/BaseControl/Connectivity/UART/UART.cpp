30f225938440d09d985af0bde1bd753ccd6250a6
Haoyu Chen <null-qwerty@outlook.com>
2024-12-16 15:12:33 +0800

    refactor(UART): change timeout limit

--- Core/Src/PHOENIX/BaseControl/Connectivity/UART/UART.cpp
+++ Core/Src/PHOENIX/BaseControl/Connectivity/UART/UART.cpp
@@ -12,12 +12,12 @@ UART::~UART(void)
 
 uint8_t UART::sendMessage()
 {
-    return HAL_UART_Transmit(huart, pSendBuffer, sendBufferSize, 0xf);
+    return HAL_UART_Transmit(huart, pSendBuffer, sendBufferSize, 20);
 }
 
 uint8_t UART::receiveMessage()
 {
-    return HAL_UART_Receive(huart, pReceiveBuffer, receiveBufferSize, 0xf);
+    return HAL_UART_Receive(huart, pReceiveBuffer, receiveBufferSize, 20);
 }
 
 UART &UART::init()
