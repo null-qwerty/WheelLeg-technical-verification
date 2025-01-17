48e9997d6b0d4b073842baaf907dde2f5cf00ba6
Haoyu Chen <null-qwerty@outlook.com>
2025-01-18 02:21:02 +0800

    feat(Connectivity): add I2C and SPI method
    add: class I2C
    add: class SPI

---
+++ Application/include/PHOENIX/BaseControl/Connectivity/SPI/SPI.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "spi.h"
+
+#include "PHOENIX/BaseControl/Connectivity/Connectivity.hpp"
+
+class SPI : public Connectivity {
+public:
+    typedef struct xSPIFrame_s {
+        uint8_t *data;
+        uint16_t length;
+    } xSPIFrame_t;
+
+    SPI(SPI_HandleTypeDef *hspi);
+    ~SPI();
+
+    virtual SPI &init() override;
+    virtual void *getReceiveFrame() override;
+    virtual void *getSendFrame() override;
+    virtual uint8_t sendMessage() override;
+    virtual uint8_t receiveMessage() override;
+    virtual uint8_t sendReceiveMessage() override;
+
+    SPI &operator=(const SPI &other);
+
+private:
+    SPI_HandleTypeDef *hspi = nullptr;
+
+    xSPIFrame_t xReceiveFrame = {};
+    xSPIFrame_t xSendFrame = {};
+};
---
+++ Application/src/PHOENIX/BaseControl/Connectivity/SPI/SPI.cpp
@@ -0,0 +1,43 @@
+#include "PHOENIX/BaseControl/Connectivity/SPI/SPI.hpp"
+
+SPI::SPI(SPI_HandleTypeDef *hspi)
+    : hspi(hspi)
+{
+}
+
+SPI::~SPI()
+{
+}
+
+SPI &SPI::init()
+{
+    return *this;
+}
+
+void *SPI::getReceiveFrame()
+{
+    return &xReceiveFrame;
+}
+
+void *SPI::getSendFrame()
+{
+    return &xSendFrame;
+}
+
+uint8_t SPI::sendMessage()
+{
+    return HAL_SPI_Transmit(hspi, xSendFrame.data, xSendFrame.length,
+                            HAL_MAX_DELAY);
+}
+
+uint8_t SPI::receiveMessage()
+{
+    return HAL_SPI_Receive(hspi, xReceiveFrame.data, xReceiveFrame.length,
+                           HAL_MAX_DELAY);
+}
+
+uint8_t SPI::sendReceiveMessage()
+{
+    return HAL_SPI_TransmitReceive(hspi, xSendFrame.data, xReceiveFrame.data,
+                                   xReceiveFrame.length, HAL_MAX_DELAY);
+}
